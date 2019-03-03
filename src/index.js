232409722695f924afbfae4b69088dd579e68397
MiguelGPereira <miguelgopereira@gmail.com>
2019-03-03 16:48:24 +0100

    webpack and babel

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function Greeting(name) {
+  console.log(`Hello ${name}!`);
+}
+
+Greeting("Miguel");
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
