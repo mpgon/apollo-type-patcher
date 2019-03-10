b270212749c25225fc0251923df316aebaae3999
Miguel Pereira <up201402671@fe.up.pt>
2019-03-10 01:32:43 +0100

    Add react integration tests (#3)

--- src/__tests__/typePatcher.ts
+++ src/__tests__/typePatcher.ts
@@ -2,6 +2,23 @@ import typePatcher from "../index";
 import mockProfessor from "../__mocks__/professor";
 import typeDefs from "../__mocks__/types";
 
+export function testProfessor(professor: any) {
+  expect(professor.department.__typename).toEqual("Department");
+  professor.classes.forEach((classVar: any) => {
+    expect(classVar.__typename).toEqual("Class");
+
+    classVar.enrolled_students.forEach((student: any) => {
+      expect(student.__typename).toEqual("Student");
+    });
+  });
+  professor.scolarship_applications.forEach((application: any) => {
+    expect(application.__typename).toEqual("ScolarshipApplication");
+    expect(application.co_assistant_professor.__typename).toEqual("Professor");
+
+    expect(application.process.insurance.__typename).toEqual("Insurance");
+  });
+}
+
 describe("typePatcher factory", () => {
   const patcher = typePatcher(typeDefs);
   const professor: any = mockProfessor();
@@ -24,21 +41,6 @@ describe("typePatcher factory", () => {
   });
 
   it("should corretly create and apply type patcher functions", () => {
-    expect(professor.department.__typename).toEqual("Department");
-    professor.classes.forEach((classVar: any) => {
-      expect(classVar.__typename).toEqual("Class");
-
-      classVar.enrolled_students.forEach((student: any) => {
-        expect(student.__typename).toEqual("Student");
-      });
-    });
-    professor.scolarship_applications.forEach((application: any) => {
-      expect(application.__typename).toEqual("ScolarshipApplication");
-      expect(application.co_assistant_professor.__typename).toEqual(
-        "Professor"
-      );
-
-      expect(application.process.insurance.__typename).toEqual("Insurance");
-    });
+    testProfessor(professor);
   });
 });
