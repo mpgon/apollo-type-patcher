40777cdf42bd7b2313c9d65802610c48e0bb9eea
mpgon <miguelgopereira@gmail.com>
2019-03-10 01:30:07 +0100

    Add react integration tests

--- src/__tests__/reactIntegration.tsx
+++ src/__tests__/reactIntegration.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 import typeDefs from "../__mocks__/types";
 import typePatcher from "../index";
-import { render } from "react-testing-library";
+import { render, wait } from "react-testing-library";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { RestLink } from "apollo-link-rest";
-import { ApolloProvider } from "react-apollo";
+import { ApolloProvider, Query } from "react-apollo";
 import { ApolloLink } from "apollo-link";
 import { withClientState } from "apollo-link-state";
+import gql from "graphql-tag";
+import mockProfessor from "../__mocks__/professor";
+import { testProfessor } from "./typePatcher";
+// Polyfills necessary for test environment
+import "@babel/polyfill";
+// @ts-ignore
+import nodeFetch from "node-fetch";
+import fetchMock from "jest-fetch-mock";
 
-// Polyfill global.Headers
-// only necessary for test environment
 // @ts-ignore
-import fetch from "node-fetch";
+global.Headers = nodeFetch.Headers;
 // @ts-ignore
-global.Headers = fetch.Headers;
+global.fetch = fetchMock;
 
 const restLink = new RestLink({
-  uri: "http://abc.com",
+  uri: "--fetch-is-mocked--",
   typePatcher: typePatcher(typeDefs)
 });
 const cache = new InMemoryCache();
@@ -31,15 +37,64 @@ const client = new ApolloClient({
   link: ApolloLink.from([stateLink, restLink])
 });
 
+const GET_PROFESSOR = gql`
+  query getProfessor($id: ID!) {
+    professor(id: $id) @rest(type: "Professor", path: "professor/{args.id}") {
+      id
+      department {
+        id
+      }
+      classes {
+        id
+        enrolled_students {
+          id
+        }
+      }
+      scolarship_applications {
+        id
+        process
+        co_assistant_professor
+      }
+    }
+  }
+`;
+
 const ApolloClientProvider = ({ children }: { children: React.ReactNode }) => (
   <ApolloProvider client={client}>{children}</ApolloProvider>
 );
 
-const App = () => <ApolloClientProvider>Hello World</ApolloClientProvider>;
+// auxiliar var to save data fetched with apollo
+let PROFESSOR_VAR: any = undefined;
 
-describe("test react", () => {
-  it("should render", () => {
+const App = () => (
+  <ApolloClientProvider>
+    <Query query={GET_PROFESSOR} variables={{ id: 1 }}>
+      {({ error, data }) => {
+        if (error) return "loading";
+        if (data && data.professor) {
+          PROFESSOR_VAR = data.professor;
+          return "loaded";
+        }
+        return "loading";
+      }}
+    </Query>
+  </ApolloClientProvider>
+);
+
+describe("test react integration", () => {
+  // @ts-ignore
+  fetch.mockResponse(JSON.stringify(mockProfessor()));
+
+  it("should type-patch query response", async () => {
     const { getByText } = render(<App />);
-    expect(getByText("Hello World")).toBeTruthy();
+
+    await wait(() => {
+      expect(getByText("loaded")).toBeTruthy();
+
+      // Apollo should inject the root typename defined in the
+      // Query component
+      expect(PROFESSOR_VAR.__typename).toEqual("Professor");
+      testProfessor(PROFESSOR_VAR);
+    });
   });
 });
