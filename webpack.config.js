13a5d051d98927d4183214ebb0cf3da855ad6491
Miguel Pereira <up201402671@fe.up.pt>
2019-03-17 19:22:23 +0100

    static checking w/ eslint+prettier (#6)

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
+/* eslint @typescript-eslint/no-var-requires: 0 */
+
 var path = require("path");
 
 module.exports = {
-  entry: "./src/index",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "app.bundle.js"
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      }
-    ]
-  }
+    entry: "./src/index",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "app.bundle.js",
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+        ],
+    },
 };
